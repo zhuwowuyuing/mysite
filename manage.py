3d9604d667bbf55f1ade98e04332afd24322f677
zhuwowuyuing <zhuwowuyuing@gmail.com>
2014-04-11 00:17:05 +0800

    setdefaultencoding utf8

--- manage.py
+++ manage.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    reload = reload(sys)
+    sys.setdefaultencoding('utf8')
+    #sys.setdefaultencoding('gb18030')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
